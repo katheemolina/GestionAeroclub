9893b7ac967e84f6d96e5e25e9447ae68f3f5bc5
Agustin Moscato <agustinmoscato369@gmail.com>
2024-11-11 21:20:28 -0300

    Agrego api de ingreso

---
+++ FrontAeroclub/src/services/ingresoApi.js
@@ -0,0 +1,22 @@
+const API_URL = 'http://localhost:8000/api';
+
+export const verificarOCrearUsuario = async (usuarioData) => {
+    try {
+        const response = await fetch(`${API_URL}/verificarUsuario`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(usuarioData),
+        });
+        
+        if (!response.ok) {
+            throw new Error('Error al insertar usuario');
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
