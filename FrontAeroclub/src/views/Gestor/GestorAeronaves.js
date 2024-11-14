77e5f489244b1bf4b52fcac7398a3fda6bb010d6
Renn4 <quote4567@gmail.com>
2024-11-13 21:50:56 -0300

    Dialog ver detalles en gestor -> aeronaves

--- FrontAeroclub/src/views/Gestor/GestorAeronaves.js
+++ FrontAeroclub/src/views/Gestor/GestorAeronaves.js
@@ -11,6 +11,9 @@ import EditIcon from '@mui/icons-material/Edit';
 import Tooltip from '@mui/material/Tooltip';
 import './Styles/GestorAeronaves.css';
 
+import SearchIcon from '@mui/icons-material/Search'; //icono de detalles
+import { Card } from 'primereact/card';
+
 
 const AeronaveCrud = () => {
     const [aeronaves, setAeronaves] = useState([]);
@@ -92,6 +95,20 @@ const AeronaveCrud = () => {
         return <span>{rowData.fecha_adquisicion}</span>;
     };
 
+    
+    // Para manejo de dialog de vista de detalles
+    const [dialogVisible, setDialogVisible] = useState(false);
+    const [selectedRowData, setSelectedRowData] = useState(null);
+    
+    const openDialog = (rowData) => {
+        setSelectedRowData(rowData);
+        setDialogVisible(true);
+    };
+    
+    const closeDialog = () => {
+        setDialogVisible(false);
+    };
+
     return (
         <div className="background">
             <header className="header">
@@ -103,22 +120,31 @@ const AeronaveCrud = () => {
                 paginator rows={10} 
                 rowsPerPageOptions={[5, 10, 25]} 
                 style={{ width: '100%' }} >
-                <Column field="marca" header="Marca"></Column>
+                {/* <Column field="marca" header="Marca"></Column> */}
                 <Column field="modelo" header="Modelo"></Column>
                 <Column field="matricula" header="Matrícula"></Column>
-                <Column field="potencia" header="Potencia (HP)"></Column>
+                {/* <Column field="potencia" header="Potencia (HP)"></Column> */}
                 <Column field="clase" header="Clase"></Column>
-                <Column field="fecha_adquisicion" header="Fecha Adquisición" body={dateTemplate}></Column>
-                <Column field="consumo_por_hora" header="Consumo por Hora" body={consumoTemplate}></Column>
-                <Column field="horas_historicas_voladas" header="Horas de vuelo" ></Column>
-                <Column field="horas_para_inspeccion" header="Horas para inspeccion" ></Column>
+                {/* <Column field="fecha_adquisicion" header="Fecha Adquisición" body={dateTemplate}></Column> */}
+                {/* <Column field="consumo_por_hora" header="Consumo por Hora" body={consumoTemplate}></Column> */}
+                {/* <Column field="horas_historicas_voladas" header="Horas de vuelo" ></Column>*/}
+                {/*<Column field="horas_para_inspeccion" header="Horas para inspeccion" ></Column> */}
                 <Column field="estado" header="Estado"></Column>
-                <Column header="Acciones" body={(rowData) => (
-                    <Tooltip title="Editar">
-                    <IconButton color="primary" aria-label="edit" onClick={() => handleEdit(rowData)}>
-                        <EditIcon />
-                    </IconButton>
-                    </Tooltip>
+                <Column header="Acciones" 
+                    body={(rowData) => (
+                    <div className='acciones'>
+                        <Tooltip title="Editar">
+                        <IconButton color="primary" aria-label="edit" onClick={() => handleEdit(rowData)}>
+                            <EditIcon />
+                        </IconButton>
+                        </Tooltip>
+                        
+                        <Tooltip title="Ver detalles">
+                        <IconButton color="primary" aria-label="view-details" onClick={() => openDialog(rowData)}>
+                            <SearchIcon />
+                        </IconButton>
+                        </Tooltip>
+                    </div>
                 )}></Column>
             </DataTable>
 
@@ -241,6 +267,53 @@ const AeronaveCrud = () => {
                         <Button label="Guardar" icon="pi pi-check" id="btn-guardar" onClick={handleSave} />
                     </div>
                 </div>
+            </Dialog>
+
+            <Dialog header="Detalles de la Aeronave" visible={dialogVisible} style={{ width: '400px' }} onHide={closeDialog}>
+                {selectedRowData && (
+                <div>
+                    <div className='p-fluid details-dialog'>
+                        <Card>
+                            <p><strong>Marca:</strong> </p>
+                            <p>{selectedRowData.marca}</p>
+                        </Card>
+                        <Card>
+                            <p><strong>Modelo:</strong> {selectedRowData.modelo}</p>
+                        </Card>
+                        <Card>
+                            <p><strong>Matrícula:</strong> {selectedRowData.matricula}</p>
+                        </Card>
+                        <Card> 
+                            <p><strong>Potencia en HP:</strong> {selectedRowData.potencia}</p>
+                        </Card>
+
+                        <Card> 
+                            <p><strong>Fecha de adquisición:</strong> {selectedRowData.fecha_adquisicion}</p>
+                        </Card>
+                        
+                        <Card> 
+                            <p><strong>Consumo por hora:</strong> {selectedRowData.consumo_por_hora}</p>
+                        </Card>
+
+                        <Card> 
+                            <p><strong>Horas de vuelo:</strong> {selectedRowData.horas_historicas_voladas}</p>
+                        </Card>
+                        
+                        <Card> 
+                            <p><strong>Horas para inspección:</strong> {selectedRowData.horas_para_inspeccion}</p>
+                        </Card>
+                        
+                        <Card> 
+                            <p><strong>Clase:</strong> {selectedRowData.clase}</p>
+                        </Card>
+                        
+                        <Card> 
+                            <p><strong>Estado:</strong> {selectedRowData.estado}</p>
+                        </Card>
+                          
+                    </div>
+                </div>
+                )}
             </Dialog>
         </div>
     );
