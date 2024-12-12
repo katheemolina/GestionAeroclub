08ace6102d175b04f915e6717f393b58a276f200
katheemolina <katheemolina@gmail.com>
2024-12-12 00:01:38 -0300

    cambio links

--- FrontAeroclub/src/components/Footer.js
+++ FrontAeroclub/src/components/Footer.js
@@ -10,9 +10,9 @@ function Footer() {
         <nav>
           <ul>
             <p>Desarrolladores:</p>
-            <li><a href="https://github.com/agusmoscato" target='blank'>Agustín Moscato</a></li>
-            <li><a href="https://github.com/Juanigpunte" target='blank'>Juan Ignacio Punte</a></li>
-            <li><a href="https://github.com/katheemolina" target='blank'>Katherine Molina</a></li>
+            <li><a href="https://www.linkedin.com/in/agustinmoscato/" target='blank'>Agustín Moscato</a></li>
+            <li><a href="https://www.linkedin.com/in/juanigpunte/" target='blank'>Juan Ignacio Punte</a></li>
+            <li><a href="https://www.linkedin.com/in/katherineluzmolinategaldi/" target='blank'>Katherine Molina</a></li>
             <li><a href="https://github.com/Renn4" target='blank'>Martín Peralta</a></li>
             <li><a href="https://github.com/ericarias30" target='blank'>Eric Andy Arias</a></li>
           </ul>
