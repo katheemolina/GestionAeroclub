8252399baba0cfda91fac514e77870bd76fa384c
Agustin Moscato <117693465+agusmoscato@users.noreply.github.com>
2025-05-18 20:13:59 -0300

    modificacion en generar cuotas sociales

--- FrontAeroclub/src/views/Gestor/GestorGenerarCuotasSociales.js
+++ FrontAeroclub/src/views/Gestor/GestorGenerarCuotasSociales.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import '../../styles/datatable-style.css';
 import PantallaCarga from '../../components/PantallaCarga';
 import { useUser } from '../../context/UserContext';
-import { generarCuotasSociales } from '../../services/generarCuotasSociales'; // Asegúrate de importar la función
+import { generarCuotasSociales, obtenerCSGeneradas } from '../../services/generarCuotasSociales'; // Asegúrate de importar la función
 import { Card } from 'primereact/card';
 import { Dialog } from 'primereact/dialog';
 import { Button } from "primereact/button";
@@ -10,6 +10,8 @@ import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './Styles/GestorGenerarCuotasSociales.css'
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 
 const GestorGenerarCuotasSociales = () => {
@@ -20,6 +22,8 @@ const GestorGenerarCuotasSociales = () => {
     const [anio, setAnio] = useState('');
     const [resultado, setResultado] = useState(null);
     const [errorState, setError] = useState('');  // Nuevo estado para manejar el error
+    const [cuotasSociales, setCuotasSociales] = useState([]);
+
     const { usuarioId } = useUser();
 
     useEffect(() => {
@@ -35,7 +39,6 @@ const GestorGenerarCuotasSociales = () => {
         setMes(mesActual);
         setAnio(anioActual);
     }, []);
-
     
     // Opciones de meses
     const mesesDelAnio = [
@@ -80,6 +83,22 @@ const GestorGenerarCuotasSociales = () => {
         }
     };
 
+    // Fetch tarifas data from the API
+    const fetchCuotasSociales = async () => {
+        try {
+            const data = await obtenerCSGeneradas();
+            //console.log(data)
+            setCuotasSociales(data.data);
+        } catch (error) {
+            console.error('Error fetching tarifas:', error);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchCuotasSociales();
+    }, []);
+
 
     if (loading) {
         return <PantallaCarga />;
@@ -90,50 +109,61 @@ const GestorGenerarCuotasSociales = () => {
             <header className="header">
                 <h1>Gestión de Cuotas Sociales</h1>
             </header>
+            <p>{cuotasSociales?.[0]?.Validacion ?? " "}</p>
             <div className='contenedor-para-centrar'>
-            <div className="form-container contenedor-generar-cuotas">
-            <label>
-                <strong>Año:</strong>
-                <input
-                    type="number"
-                    min="2000"
-                    max="2100"
-                    value={anio}
-                    onChange={(e) => setAnio(e.target.value)}
-                    placeholder="Ejemplo: 2025"
-                />
-            </label>
-            <label>
-                <strong>Mes:</strong>
-                <select value={mes} onChange={(e) => setMes(e.target.value)}>
-                    <option value="" disabled>
-                        Seleccione un mes
-                    </option>
-                    {mesesDelAnio.map((mesOption) => (
-                        <option key={mesOption.value} value={mesOption.value}>
-                            {mesOption.label}
-                        </option>
-                    ))}
-                </select>
-            </label>
-            <label>
-                <strong>Importe:</strong>
-                <input
-                    type="number"
-                    value={importe}
-                    onChange={(e) => setImporte(e.target.value)}
-                    placeholder="Ejemplo: 1000"
-                />
-            </label>
-            <button
-                id="btn-generar-cuotas"
-                className="btn-primary"
-                onClick={() => setShowConfirmDialog(true)}
-            >
-                Generar Cuotas
-            </button>
-        </div>
-
+                <div className="form-container contenedor-generar-cuotas">
+                    <label>
+                        <strong>Año:</strong>
+                        <input
+                            type="number"
+                            min="2000"
+                            max="2100"
+                            value={anio}
+                            onChange={(e) => setAnio(e.target.value)}
+                            placeholder="Ejemplo: 2025"
+                        />
+                    </label>
+                    <label>
+                        <strong>Mes:</strong>
+                        <select value={mes} onChange={(e) => setMes(e.target.value)}>
+                            <option value="" disabled>
+                                Seleccione un mes
+                            </option>
+                            {mesesDelAnio.map((mesOption) => (
+                                <option key={mesOption.value} value={mesOption.value}>
+                                    {mesOption.label}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
+                    <label>
+                        <strong>Importe:</strong>
+                        <input
+                            type="number"
+                            value={importe}
+                            onChange={(e) => setImporte(e.target.value)}
+                            placeholder="Ejemplo: 1000"
+                        />
+                    </label>
+                    <button
+                        id="btn-generar-cuotas"
+                        className="btn-primary"
+                        onClick={() => setShowConfirmDialog(true)}
+                    >
+                        Generar Cuotas
+                    </button>
+                </div>
+                <div className="form-container contenedor-generar-cuotas">
+                    <DataTable 
+                        filterDisplay='row'
+                        value={cuotasSociales} 
+                        paginator rows={10} 
+                        rowsPerPageOptions={[5, 10, 25]} 
+                        style={{ width: '100%' }} >
+                        <Column field="periodo" header="Periodo mm/aaaa" ></Column>
+                        <Column field="CantidadCSGeneradas" header="Cantidad de Cuotas Sociales Generadas" ></Column>
+                    </DataTable>
+                </div>
             </div>
 
             <Dialog header="Confirmar" className="modal-confirmar-habilitacion" visible={showConfirmDialog} style={{ width: '350px' }} modal footer={
